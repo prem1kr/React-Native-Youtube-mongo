b6047ece3c620eee9b20d00e8ee6742533e68279
prem kumar <151729765+prem1kr@users.noreply.github.com>
2025-09-10 17:26:41 +0530

    Update useFetchVideo.js

--- frontend/hooks/useFetchVideo.js
+++ frontend/hooks/useFetchVideo.js
@@ -2,13 +2,15 @@ import { fetchVideos } from "@/services/api";
 import { useState, useEffect } from "react";
 
 export function useFetchVideos() {
-  const [videos, setVideos] = useState({ videos: [] }); 
+  const [videos, setVideos] = useState([]); 
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetchVideos()
-      .then((data) => setVideos(data))
-      .catch((err) => console.error(err))
+      .then((data) => {
+        setVideos(Array.isArray(data) ? data : data?.videos || []);
+      })
+      .catch((err) => console.error("Error fetching videos:", err))
       .finally(() => setLoading(false));
   }, []);
 
